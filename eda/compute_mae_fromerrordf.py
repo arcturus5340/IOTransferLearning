1dff6492d89ad1a1408c8236098115c800f573be
arcturus5340 <arcturus5340@gmail.com>
2025-06-08 16:42:09 +0200

    Run all EDA notebooks to finalize outputs

--- eda/compute_mae_fromerrordf.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-
-df = pd.read_csv(Path(r"/home/rwth1591/transfer-learning/blue_waters/data/blue_waters_dataset_processed/blue_waters_posix_withapps_no_negative_outliers_no_time_witherrors.csv"))
-
-mae = (df['error'].abs() / df['mean']).median()
-print(mae)
